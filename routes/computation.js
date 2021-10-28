d727b7eef9b6a91ff9c056ab57971e313a855443
NarendraUnnava <89415190+NarendraUnnava@users.noreply.github.com>
2021-10-27 23:00:39 -0500

    Added Bonus

---
+++ routes/computation.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+
+
+/* GET users listing. */
+router.get('/', function (req, res, next) {
+    fake_url = "https://fake.com/path" + req.url
+    const url = new URL(fake_url)
+    const search_params = url.searchParams
+    if (req.method === 'GET') {
+        value = search_params.get("x")
+        if(value === null)
+        {
+            value=Math.round(Math.random()*900);
+        }
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write('Math.sinh() applied to ' + value + ' is ' + Math.sinh(value))
+        res.end()
+    }
+});
+
+module.exports = router;
